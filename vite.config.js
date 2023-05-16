664fca9bc016bd5d6da5a9e769765cbc09c52764
Victor HONG <victor81528@hotmail.com>
2023-05-16 19:22:18 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/pdf-editor/',
+  base: process.env.NODE_ENV === 'production' ? '/pdf-editor/' : '/',
   plugins: [vue()]
 })
